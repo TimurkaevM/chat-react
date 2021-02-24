a2de53724c54f894b00d4b45648781d44b1930b9
SiriKey <kiryu110@mail.ru>
2021-02-24 12:18:50 +0200

    output of messages

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-
-import style from './style.module.css';
+// import style from './style.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from './Message';
+import { loadMessages } from '../../redux/messages';
+import { useEffect } from 'react';
 
 function Messages() {
-  return <div></div>;
+  const messages = useSelector((state) => state.messages.items);
+  const loading = useSelector((state) => state.messages.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadMessages());
+  }, []);
+
+  if (loading) {
+    return <div>loading messages...</div>;
+  }
+  return (
+    <div>
+      {messages.map((message) => {
+        return <Message key={messages.id} message={message} />;
+      })}
+    </div>
+  );
 }
 
 export default Messages;
