bf4d266d8aeeaa30f17410efd57a66c2db765cc9
emin <magomedemin.islamov@mail.ru>
2021-02-25 20:27:12 +0300

    added new icon and last messages

--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -4,8 +4,21 @@ import { NavLink } from 'react-router-dom';
 
 function Contact(props) {
   return (
-    <NavLink to={`/${props.contact._id}`} className={style.contact}>
-      {props.contact.fullname}
+    <NavLink
+      to={`/${props.contact._id}`}
+      className={style.contact}
+      activeClassName={style.activeContact}
+    >
+      {/*<i className={`${style.materialIcons} material-icons md-48`}>stop_circle</i>*/}
+      <div className={style.circle}>{props.contact.fullname[0]}</div>
+      <div>
+        {props.contact.fullname}
+        <div>
+          {props.contact.lastMessage === undefined
+            ? ''
+            : props.contact.lastMessage.content}
+        </div>
+      </div>
     </NavLink>
   );
 }
