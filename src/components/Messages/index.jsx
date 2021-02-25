585fd2b641bfb5366390a6d197da8f9ef46201e2
SiriKey <kiryu110@mail.ru>
2021-02-25 19:21:45 +0200

    fixed key

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-// import style from './style.module.css';
+import style from './style.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import Message from './Message';
 import { loadMessages } from '../../redux/messages';
 import { useEffect } from 'react';
+import Message from './Message';
+import { useParams } from 'react-router-dom';
 
 function Messages() {
+  const params = useParams();
   const messages = useSelector((state) => state.messages.items);
   const loading = useSelector((state) => state.messages.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadMessages());
-  }, []);
+    if (params.id !== undefined) {
+      dispatch(loadMessages(params.id));
+    }
+  }, [params]);
 
   if (loading) {
     return <div>loading messages...</div>;
