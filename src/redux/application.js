96feb1a3f28eb54fbdcc605e1eadacc8291d1841
Muhammad <timurkaevmuhammad4@gmail.com>
2021-02-28 03:47:21 +0300

    get profile

--- src/redux/application.js
+++ src/redux/application.js
@@ -1,6 +1,43 @@
-export default (state = {}, action) => {
+const LOAD = 'user/load/start';
+const SUCCESS = 'user/load/saccess';
+
+const initialState = {
+  items: {},
+  loading: false,
+};
+
+export default (state = initialState, action) => {
   switch (action.type) {
+    case LOAD:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case SUCCESS:
+      return {
+        ...state,
+        items: action.payLoad,
+      };
+
     default:
       return state;
   }
 };
+
+export function getUser() {
+  return (dispatch) => {
+    dispatch({
+      type: LOAD,
+    });
+
+    fetch('https://api.intocode.ru:8001/api/profile')
+      .then((response) => response.json())
+      .then((json) => {
+        dispatch({
+          type: SUCCESS,
+          payLoad: json,
+        });
+      });
+  };
+}
