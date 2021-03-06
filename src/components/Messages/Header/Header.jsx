a5a27dc834d78954e19cf621e42fe2aa5f7e9b78
SiriKey <kiryu110@mail.ru>
2021-03-06 08:52:05 +0200

    added onclick for profile-icon

--- src/components/Messages/Header/Header.jsx
+++ src/components/Messages/Header/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import UserName from './UserName';
 import style from '../style.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import MessageSearch from './MessageSearch';
+import { openQuestionnaire } from '../../../redux/application';
 
 function Header(props) {
   const params = useParams().id;
+  const dispatch = useDispatch();
 
   const profileName = useSelector((state) =>
     state.contacts.items.find((i) => {
       return params === i._id;
     }),
   );
+
+  const handleClick = () => {
+    dispatch(openQuestionnaire());
+  };
   return (
     <div className={style.header}>
       <div className={style.header_search}>
@@ -23,7 +29,9 @@ function Header(props) {
         online={profileName?.online}
       />
       <div className={style.profile}>
-        <i className="material-icons click">person</i>
+        <i className="material-icons click" onClick={handleClick}>
+          person
+        </i>
       </div>
     </div>
   );
