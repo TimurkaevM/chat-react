251fff12a58c5ecab22f1ff07198690f93c23967
SiriKey <kiryu110@mail.ru>
2021-03-12 13:30:13 +0200

    added another proptypes and preloader for chat

--- src/components/Messages/Header/UserName.jsx
+++ src/components/Messages/Header/UserName.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import style from '../style.module.css';
+import PropTypes from 'prop-types';
+
 function UserName({ profileName, online }) {
   return (
     <div className={style.header_name}>
@@ -10,5 +12,10 @@ function UserName({ profileName, online }) {
     </div>
   );
 }
+
+UserName.propTypes = {
+  profileName: PropTypes.string,
+  online: PropTypes.bool,
+};
 
 export default UserName;
--- src/components/Messages/MessageChecker.jsx
+++ src/components/Messages/MessageChecker.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import OutgoingMessage from './OutgoingMessage';
 import IncomingMessages from './IncomingMessages';
+import PropTypes from 'prop-types';
 
 function MessageChecker({ message }) {
   const myId = useSelector((state) => state.application.items._id);
@@ -10,5 +11,9 @@ function MessageChecker({ message }) {
   }
   return <IncomingMessages message={message} />;
 }
+
+MessageChecker.propTypes = {
+  message: PropTypes.object,
+};
 
 export default MessageChecker;
